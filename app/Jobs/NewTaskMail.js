e75b54aedca55c393ab6b9815cda27b9cb9f667d
m7Aei <mateuspm2015@hotmail.com>
2019-04-15 23:57:49 -0300

    Commit inicial, upando todo o projeto no git

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+
+    await Mail.send(
+      ['emails.new_task'],
+      { username, title, hasAttachement: !!file },
+      message => {
+        message
+          .to(email)
+          .from('mateuspm2015@hotmail.com', 'Mateus | NodeJS')
+          .subject('Nova tarefa para você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
